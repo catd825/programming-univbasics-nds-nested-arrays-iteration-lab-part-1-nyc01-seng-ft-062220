b70d0ee7a1fc3aa763576d4bef29221b09f7fe05
Cathy D'Onofrio <catd825@gmail.com>
2020-05-25 23:24:34 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,8 +6,8 @@ def find_even_values(src)
   row_index = 0
   while row_index < src.length do #find length of outer array, compare it to row_index
     element_index = 0 #create new variable - set to zero every time outer loop runs
-    while element_index < src[row_index].length do #gives us length of current nested array 
-      if src[row_index][element_index].even? #if array within array is even, return value
+    while element_index < src[row_index].length do #runs for the length of current nested array 
+      if src[row_index][element_index].even? #if element within inner array is even
       p src[row_index][element_index] #return value if condition is met
     end
       element_index +=1 #increment through inner loop
